87a545bda0d23329cdc072fa8890cecb0a2fee1c
Stas <shadrin060388@gmail.com>
2024-01-10 00:14:11 +0300

    Solution task 2

--- src/day1/Task2.java
+++ src/day1/Task2.java
@@ -1,8 +1,26 @@
 package day1;
 
+/** Вывести на экран слово “​JAVA”​, в столбик, чтобы оно повторилось 10 раз,
+ используя цикл на ваше усмотрение (​for​ или w​hile​).
+ Вывод в консоль должен быть таким:
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ JAVA
+ */
 public class Task2 {
     public static void main(String[] args) {
-
+        int count = 0;
+        while (count < 10){
+            System.out.println("JAVA ");
+            count++;
+        }
 
     }
 }
