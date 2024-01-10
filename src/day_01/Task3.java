f39c859f6e1e563061ebd0d2a635e8f28220c4e3
Stas <shadrin060388@gmail.com>
2024-01-10 12:23:59 +0300

    Solution task 3

--- src/day_01/Task3.java
+++ src/day_01/Task3.java
@@ -1,7 +1,11 @@
-package day1;
+package day_01;
 
+/** Повторить задание 1, но используя цикл for.*/
 public class Task3 {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("JAVA ");
+        }
 
     }
 }
