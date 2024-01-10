82b4bac10164b057df6b22c05f0f0e8eacd00e64
Stas <shadrin060388@gmail.com>
2024-01-10 22:00:32 +0300

    Solution task 3 day 2

--- src/day_02/Task3.java
+++ src/day_02/Task3.java
@@ -1,9 +1,27 @@
-package day2;
+package day_02;
+
+/**
+ * Реализовать программу №2, используя цикл while.
+ */
 
 import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите два числа: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int i = a + 1;
+        if (a < b){
+            while (i < b){
+                if (i % 5 == 0 && i % 10 != 0){
+                    System.out.print(i + " ");
+                }
+                i++;
+            }
+        } else {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
