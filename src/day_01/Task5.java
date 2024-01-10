6a2b50e447f9ba15b169333a07c36ef9e0bb3b4a
Stas <shadrin060388@gmail.com>
2024-01-10 12:37:07 +0300

    Solution task 5

--- src/day_01/Task5.java
+++ src/day_01/Task5.java
@@ -1,7 +1,11 @@
-package day1;
+package day_01;
 
+/** Повторить задание 4, но используя цикл for.*/
 public class Task5 {
     public static void main(String[] args) {
-
+        int year = 1980;
+        for (int i = year; i <= 2020; i += 4) {
+            System.out.println("Олимпиада " + i + " года");
+        }
     }
 }
