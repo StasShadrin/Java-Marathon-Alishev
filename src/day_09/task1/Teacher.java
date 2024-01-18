8f49046c19ef25b2be2373fa4f6da0eb40834656
Stas <shadrin060388@gmail.com>
2024-01-18 14:11:14 +0300

    Solution task 1 day 9

---
+++ src/day_09/task1/Teacher.java
@@ -0,0 +1,23 @@
+package day_09.task1;
+
+public class Teacher extends Human {
+    private String subject;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Teacher(String name, String subject) {
+        super(name);
+        this.subject = subject;
+    }
+    @Override
+    public void printInfo(){
+        super.printInfo();
+        System.out.println("Этот преподаватель с именем " + name);
+    }
+}
